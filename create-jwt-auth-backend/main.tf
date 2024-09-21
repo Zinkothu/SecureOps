862751d1be15e6b60758eec14ecf5aa0fd9970eb
Zinkothu <mr.zinkothu@gmail.com>
2024-09-21 20:21:04 +0800

    remote OIDC configuration in config

--- create-jwt-auth-backend/main.tf
+++ create-jwt-auth-backend/main.tf
@@ -2,7 +2,7 @@
 resource "vault_jwt_auth_backend" "main" {
     description         = "Terraform Dynamic Credential Provider"
     path                = "jwt"
-    oidc_discovery_url  = "https://app.terraform.io/"
+    #oidc_discovery_url  = "https://app.terraform.io/"
     bound_issuer        = "https://app.terraform.io/"
 }
 
