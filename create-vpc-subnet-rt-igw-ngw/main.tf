4947c8d873f5f673c16fb1adbf6b2a25de210e54
Zinkothu <mr.zinkothu@gmail.com>
2024-09-26 22:08:45 +0800

    add db and private and configure gateway

--- create-vpc-subnet-rt-igw-ngw/main.tf
+++ create-vpc-subnet-rt-igw-ngw/main.tf
@@ -15,12 +15,40 @@ resource "aws_subnet" "public_subnets" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index] #count.index is will call index number 0 then 1 and 2 
   availability_zone  =  data.aws_availability_zones.azs.names[count.index]
-  map_public_ip_on_launch = true  #public ip assign
-
+  map_public_ip_on_launch = true  #public ip 
   tags = {
     Name = "public-subnet-0${count.index+1}-${data.aws_availability_zones.azs.names[count.index]}"
   }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets) 
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public_rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.public_igw.id
+}
+
+resource "aws_internet_gateway" "public_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-igw"
+  }
+}
+
 
 ################################################################################
 # Private Subnets
@@ -31,11 +59,49 @@ resource "aws_subnet" "private_subnets" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnets[count.index] #count.index is will call index number 0 then 1 and 2 
   availability_zone  =  data.aws_availability_zones.azs.names[count.index]
-  map_public_ip_on_launch = true  #public ip assign
+  map_public_ip_on_launch = false  #public ip assign
 
   tags = {
     Name = "private-subnet-0${count.index+1}-${data.aws_availability_zones.azs.names[count.index]}"
   }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets) 
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private_rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+ nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+  depends_on   = [aws_internet_gateway.public_igw]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.private_igw]
 }
 
 ################################################################################
@@ -47,10 +113,29 @@ resource "aws_subnet" "db_subnets" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.db_subnets[count.index] #count.index is will call index number 0 then 1 and 2 
   availability_zone  =  data.aws_availability_zones.azs.names[count.index]
-  map_public_ip_on_launch = true  #public ip assign
+  map_public_ip_on_launch = false  #public ip assign
 
   tags = {
     Name = "db-subnet-0${count.index+1}-${data.aws_availability_zones.azs.names[count.index]}"
   }
 }
 
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "db-route-table"
+  }
+}
+
+resource "aws_route_table_association" "db" {
+  count = length(var.db_subnets) 
+  subnet_id      = aws_subnet.db_subnets[count.index].id
+  route_table_id = aws_route_table.db_rt.id
+}
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private_rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.db_igw.id
+}
