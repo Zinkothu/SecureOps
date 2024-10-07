ce3784c2da31eeb9a1eb78b8978abddfaf79c3ba
Zinkothu <mr.zinkothu@gmail.com>
2024-10-07 22:31:57 +0800

    update config with their name

--- 04-create-aws-secret-dynamic-role/main.tf
+++ 04-create-aws-secret-dynamic-role/main.tf
@@ -2,7 +2,7 @@ resource "vault_aws_secret_backend" "aws" {
   access_key = aws_iam_access_key.vault_root.id
   secret_key = aws_iam_access_key.vault_root.secret
   region = "ap-southeast-1"
-  path = "aws-iam-admin"
+  path = "aws-iam-admin" #aws secret engine custom path name
   default_lease_ttl_seconds =  900
   max_lease_ttl_seconds = 2000
 }
@@ -12,7 +12,7 @@ resource "vault_aws_secret_backend_role" "role" {
   name    = "admin-role"
   credential_type = "iam_user"
 
-  policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+  policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"] #this policy refer to later vault create iam user will get this policy
 }
 
 data "vault_aws_access_credentials" "creds" {
