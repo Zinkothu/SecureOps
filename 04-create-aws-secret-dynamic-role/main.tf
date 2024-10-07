bac18521f3d370cabcbf44159c4a627cdf713c5a
Zinkothu <mr.zinkothu@gmail.com>
2024-10-08 00:26:03 +0800

    update

--- 04-create-aws-secret-dynamic-role/main.tf
+++ 04-create-aws-secret-dynamic-role/main.tf
@@ -2,7 +2,7 @@ resource "vault_aws_secret_backend" "aws" {
   access_key = aws_iam_access_key.vault_root.id
   secret_key = aws_iam_access_key.vault_root.secret
   region = "ap-southeast-1"
-  path = "aws-iam-admin" #aws secret engine custom path name
+  path = "aws-iam-admin"
   default_lease_ttl_seconds =  900
   max_lease_ttl_seconds = 2000
 }
