e33776d41f49f6c1f4fb2ac34c281150656dc5a4
Zinkothu <mr.zinkothu@gmail.com>
2024-10-17 20:13:36 +0800

    update

--- 01-tfc-hcp-vault-cluster/outputs.tf
+++ 01-tfc-hcp-vault-cluster/outputs.tf
@@ -1,7 +1,34 @@
+# Outputs file
+output "vault_private_endpoint" {
+  value =  hcp_vault_cluster.aws_hcp_vault_cluster.vault_private_endpoint_url
+}
+
+output "vault_namespace" {
+  value =  hcp_vault_cluster.aws_hcp_vault_cluster.namespace
+}
+
+output "vault_version" {
+  value =  hcp_vault_cluster.aws_hcp_vault_cluster.vault_version
+}
+
 output "hvn_id" {
   description = "HCP HVN ID"
-  value = hcp_hvn.vault_hvn
+  value = hcp_hvn.aws_hcp_vault_hvn
 }
+
+# output "role_id" {
+#   value = vault_approle_auth_backend_role.tfc_approle.role_id
+# }
+
+# output "secret_id" {
+#   value = nonsensitive(vault_approle_auth_backend_role_secret_id.tfc_secrect_id.secret_id)
+# }
+
+# output "role_path" {
+#   value = vault_auth_backend.approle.path
+# }
+
+
 
 output "vault_id" {
   description = "HCP Vault Cluster ID"
