ba3da52d49877ac883dab153fbb3a6015870c5e4
Zinkothu <mr.zinkothu@gmail.com>
2024-10-21 22:09:37 +0800

    update output file from vault cluster workspace

--- 01-tfc-hcp-vault-cluster/outputs.tf
+++ 01-tfc-hcp-vault-cluster/outputs.tf
@@ -16,7 +16,7 @@ output "vault_version" {
 
 output "hvn_id" {
   description = "HCP HVN ID"
-  value = hcp_hvn.aws_hcp_vault_hvn
+  value = hcp_hvn.aws_hcp_vault_hvn.hvn_id
 }
 
 output "vault_id" {
@@ -24,14 +24,18 @@ output "vault_id" {
   value = hcp_vault_cluster.aws_hcp_vault_cluster.cluster_id
 }
 
-# output "role_id" {
-#   value = vault_approle_auth_backend_role.tfc_approle.role_id
+# output "vault_admin_token_id" {
+#   description = "HCP Vault Admin Token ID"
+#   value = hcp_vault_cluster_admin_token.vault_admin_token.id
 # }
 
-# output "secret_id" {
-#   value = nonsensitive(vault_approle_auth_backend_role_secret_id.tfc_secrect_id.secret_id)
+# output "vault_admin_token" {
+#   description = "HCP Vault Admin Token"
+#   value = hcp_vault_cluster_admin_token.vault_admin_token.token
+#   sensitive = true
 # }
 
-# output "role_path" {
-#   value = vault_auth_backend.approle.path
+# output "vault_private_endpoint_url" {
+#   description = "HCP Vault Cluster vault_public_endpoint_url"
+#   value = hcp_vault_cluster.vault_cluster.vault_private_endpoint_url
 # }
